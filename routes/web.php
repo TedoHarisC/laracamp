38c73c988c9ca31b641382abdfdc36983846f4af
TedoHarisC <tedoharischandra29@gmail.com>
2023-04-13 09:59:49 +0700

    Admin Middleware

--- routes/web.php
+++ routes/web.php
@@ -37,21 +37,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Checkout Routes
-    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
-    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
-    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success')->middleware('ensureUserRole:user');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create')->middleware('ensureUserRole:user');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store')->middleware('ensureUserRole:user');
 
     // Dashboard
     Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
     //Route::get('dashboard/checkout/invoice/{checkout}', [CheckoutController::class, 'invoice'])->name('user.checkout.invoice');
 
     // User Dashboard
-    Route::prefix('user/dashboard')->namespace('User')->name('user.')->group(function(){
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('ensureUserRole:user')->group(function(){
         Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
     });
 
     // Admin Dashboard
-    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->group(function(){
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('ensureUserRole:admin')->group(function(){
         Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
     });
 });
