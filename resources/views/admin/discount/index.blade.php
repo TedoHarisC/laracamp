9746c3b119398c7d88dcd754102e37bad8ed2f31
IT Site ABB <itsiteabb@ITs-MacBook-Pro.local>
2023-09-28 10:11:01 +0700

    Menampilkan list discount

--- resources/views/admin/discount/index.blade.php
+++ resources/views/admin/discount/index.blade.php
@@ -9,55 +9,51 @@
                     Discount
                 </div>
                 <div class="card-body">
-                    <div class="row">
+                    <div class="row mb-3">
                         <div class="col-md-12 d-flex flex-row-reverse">
                             <a href="{{route('admin.discount.create')}}" class="btn btn-primary btn-sm">Add Discount</a>
                         </div>
                     </div>
                     @include('components.alert')
-                    {{-- <table class="table">
+                    <table class="table">
                         <thead>
                             <tr>
-                                <th>User</th>
-                                <th>Camp</th>
-                                <th>Price</th>
-                                <th>Register Data</th>
-                                <th>Paid Status</th>
+                                <th>Name</th>
+                                <th>Code</th>
+                                <th>Description</th>
+                                <th>Percentage</th>
+                                <th colspan="2">Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @forelse ($checkouts as $checkout)
+                            @forelse ($discounts as $discount)
                             <tr>
-                                <td>{{$checkout->User->name}}</td>
-                                <td>{{$checkout->Camp->title}}</td>
-                                <td>{{$checkout->Camp->price}}k</td>
-                                <td>{{$checkout->created_at->format('M d Y')}}</td>
+                                <td>{{$discount->name}}</td>
                                 <td>
-                                    <strong>{{$checkout->payment_status}}</strong>
+                                    <span class="badge bg-primary">
+                                        {{$discount->code}}
+                                    </span>
                                 </td>
-                                {{-- <td>
-                                    @if ($checkout->is_paid)
-                                    <span class="badge bg-success">Paid</span>
-                                    @else
-                                    <span class="badge bg-warning">Waiting</span>
-                                    @endif
-                                </td> --}}
-                                {{-- <td>
-                                    @if (!$checkout->is_paid)
-                                    <form action="{{route('admin.checkout.update', $checkout->id)}}" method="post">
+                                <td>{{$discount->description}}k</td>
+                                <td>{{$discount->percentage}}%</td>
+                                <td>
+                                    <a href="{{route('admin.discount.edit', $discount->id)}}" class="btn btn-warning">Edit</a>
+                                </td>
+                                <td>
+                                    <a href="{{route('admin.discount.destroy', $discount->id)}}" method="post">
                                         @csrf
-                                        <button class="btn btn-primary btn-sm">Set to Paid</button>
-                                    </form>
-                                    @endif
+                                        @method('DELETE')
+                                        <button class="btn btn-danger">Delete</button>
+                                    </a>
                                 </td>
                             </tr>
                             @empty
                             <tr>
-                                <td colspan="3">No camps registered</td>
+                                <td colspan="6">No discount created</td>
                             </tr>
                             @endforelse
                         </tbody>
-                    </table> --}}
+                    </table>
                 </div>
             </div>
         </div>
